351871133324a561dc31ff41fc5e238dfe88bc32
Anastasia Vikhlyaeva <36508478+anasvikh@users.noreply.github.com>
2020-12-14 22:30:59 +0300

    Update RoundResultsScreen.tsx

--- src/app/components/round-results-screen/RoundResultsScreen.tsx
+++ src/app/components/round-results-screen/RoundResultsScreen.tsx
@@ -46,7 +46,7 @@ export class RoundResultsScreen extends Component<RoundResultsScreenProps> {
                                         <div className="card-number">{this.props.results.activePlayCard.numberInSet}</div>}
                                     <img
                                         className="card active-card"
-                                        src={`${process.env.REACT_APP_API_URL}${this.props.results.activePlayCard.src}`}></img>
+                                        src={`${this.props.results.activePlayCard.src}`}></img>
                                 </div>
                             </div>
                         </Zoom>
